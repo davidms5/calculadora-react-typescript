d935bdfe022adb3c0be6911e0d93d294a456b703
david medina <rafaelsanchez97.dm@gmail.com>
2023-02-15 01:04:02 -0300

    arreglado el styled components

--- src/App.style.ts
+++ src/App.style.ts
@@ -1,3 +1,13 @@
 import styled ,{createGlobalStyle} from 'styled-components';
 
-export default Container = styled``
+export const Container = styled.div`
+max-width: 500px;
+min-width: 300px;
+margin: 0 auto;
+user-select: none;
+
+@media screen and (min-width:450px){
+    min-width: 340px
+}
+
+`
--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import GlobalStyle1 from './styles/tema1'
 import GlobalStyle2 from './styles/tema2'
 import GlobalStyle3 from './styles/tema3'
 import './App.css'
+import {Container} from './App.style'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
+    <Container>
+      <h1>hola mundo</h1>
+    </Container>
     </>
   )
 }
